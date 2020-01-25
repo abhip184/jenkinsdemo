8a042104695d5e480e5a23be227002a2ad00fdca
abhip184 <abhip@zignuts.com>
2020-01-25 17:45:41 +0530

    surname added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Abhi
+          Learn React With Abhi Patel
         </a>
       </header>
     </div>
